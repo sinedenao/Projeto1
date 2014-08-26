c46e3c60108399983c18568f366b58551fdb2ff0
sinedenao <denis.depaula@gmail.com>
2014-08-25 22:53:03 -0300

    Adicionando Classe Cliente
    Adicionando Classe Cliente

---
+++ Cliente.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinedenao
+ * Date: 25/08/14
+ * Time: 22:46
+ */
+
+class Cliente {
+
+    private $db;
+
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function listar()
+    {
+
+
+    }
+
+    public function inserir()
+    {
+
+
+    }
+
+    public function alterar()
+    {
+
+
+    }
+
+    public function deletar()
+    {
+
+
+    }
+
+} 
