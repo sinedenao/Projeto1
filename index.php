575a2aa663d3569ee196514df9e5d89feda3860b
sinedenao <denis.depaula@gmail.com>
2014-08-25 23:21:24 -0300

    Crud, Aula Inserir.
    Crud, Aula Inserir.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'Cliente.php';
+
+try{
+    $conexao = new \PDO("mysql:host=localhost:3306;dbname=pdo","root","root");
+
+}
+catch (Exception $e)
+{
+    //Para execucao do programa
+    die($e->getMessage());
+}
+$cliente = new Cliente($conexao);
+$cliente->setNome('Denis')
+        ->setEmail('denis@teste.com.br');
+$resultado = $cliente->inserir();
+
+echo $resultado;
