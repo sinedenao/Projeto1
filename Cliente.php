2ae3f2fd0fad67073054d8839ec751cf9ddb654f
sinedenao <denis.depaula@gmail.com>
2014-08-25 23:21:16 -0300

    Crud, Aula Inserir.
    Crud, Aula Inserir.

--- Cliente.php
+++ Cliente.php
@@ -9,6 +9,10 @@
 class Cliente {
 
     private $db;
+    private $id;
+    private $nome;
+    private $email;
+
 
     public function __construct(\PDO $db)
     {
@@ -18,12 +22,19 @@ class Cliente {
     public function listar()
     {
 
-
     }
 
     public function inserir()
     {
+        $query = "insert into clientes (nome,email) values (:nome,:email)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome',$this->getNome());
+        $stmt->bindValue(':email',$this->getEmail());
 
+        if($stmt->execute())
+        {
+            return true;
+        }
 
     }
 
@@ -38,5 +49,75 @@ class Cliente {
 
 
     }
+
+    /**
+     * @param \PDO $db
+     */
+    public function setDb($db)
+    {
+        $this->db = $db;
+        return $this;
+    }
+
+    /**
+     * @return \PDO
+     */
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+
 
 } 
